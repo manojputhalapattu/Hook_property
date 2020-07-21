6f6b4bf59d8a5899b7ec40085a42fc34c1119634
manoj puthalapattu <58524654+manojputhalapattu@users.noreply.github.com>
2020-07-21 11:50:20 +0530

    "setCount-update"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 
 function App() {
-  const state = useState(123);
+  const [state,setCount] = useState(1);
   function increase() {
-    // count++
+    // count++7
+    // state++
+    setCount(state+1)
   }
+  function decrease() {
+    // count++7
+    // state++
+    setCount(state-1)
+  }
+
   // console.log(count);
   return (
     <div className="container">
       {/* <h1>{count}</h1> */}
       <h1>{state}</h1>
       <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
