19680a727e99fe111364bb8b85d4f387335f2eb0
manoj puthalapattu <58524654+manojputhalapattu@users.noreply.github.com>
2020-07-21 10:19:22 +0530

    "Re-Rendering"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+var count=0;
+
+function increase(){
+  count++
+  // console.log(count);
+  ReactDOM.render(
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+    </div>,
+    document.getElementById("root")
+  );
+  
+}
+
 ReactDOM.render(
   <div className="container">
-    <h1>0</h1>
-    <button>+</button>
+    <h1>{count}</h1>
+    <button onClick={increase}>+</button>
   </div>,
   document.getElementById("root")
 );
